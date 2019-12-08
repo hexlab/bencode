f5a5eda7bc501f243262640e0d02b664b381b553
Kirill Karmadonov <kirill@live.com>
2019-12-08 11:49:23 +0200

    Rewrite decoder

--- bencode/decode.py
+++ bencode/decode.py
@@ -2,39 +2,29 @@ SEP, INT, DCT, LST, END = b':idle'
 NUM = b'0123456789'
 
 
-def parse_int(bstring: bytes, indx: int) -> (int, int):
-    indx, end_indx = indx + 1, bstring.index(END, indx)
-    return int(bstring[indx:end_indx]), end_indx + 1
+def decode(bstring: bytes, indx: int) -> (bytes, int):
+    byte = bstring[indx]
+    if byte == INT:
+        indx, end_indx = indx + 1, bstring.index(END, indx)
+        return int(bstring[indx:end_indx]), end_indx + 1
+    elif byte == LST:
+        indx, lst = indx + 1, []
+        while bstring[indx] != END:
+            v, indx = decode(bstring, indx)
+            lst.append(v)
+        return lst, indx + 1
+    elif byte == DCT:
+        indx, dct = indx + 1, {}
+        while bstring[indx] != END:
+            key, indx = decode(bstring, indx)
+            dct[key], indx = decode(bstring, indx)
+        return dct, indx + 1
+    else:
+        sep_indx = bstring.index(SEP, indx)
+        length = int(bstring[indx:sep_indx])
+        sep_indx += 1
+        return bstring[sep_indx:sep_indx + length], sep_indx + length
 
 
-def parse_string(bstring: bytes, indx: int) -> (bytes, int):
-    sep_indx = bstring.index(SEP, indx)
-    length = int(bstring[indx:sep_indx])
-    sep_indx += 1
-    return bstring[sep_indx:sep_indx + length], sep_indx + length
-
-
-def parse_list(bstring: bytes, indx: int) -> (list, int):
-    indx, lst = indx + 1, []
-    while bstring[indx] != END:
-        v, indx = parsers[bstring[indx]](bstring, indx)
-        lst.append(v)
-    return lst, indx + 1
-
-
-def parse_dict(bstring: bytes, indx: int) -> (dict, int):
-    indx, dct = indx + 1, {}
-    while bstring[indx] != END:
-        key, indx = parse_string(bstring, indx)
-        dct[key], indx = parsers[bstring[indx]](bstring, indx)
-    return dct, indx + 1
-
-
-parsers = {i: parse_string for i in NUM}
-parsers[LST] = parse_list
-parsers[DCT] = parse_dict
-parsers[INT] = parse_int
-
-
-def bdecode(bstring: bytes):
-    return parsers[bstring[0]](bstring, 0)[0]
+def bdecode(bstring):
+    return decode(bstring, 0)[0]
