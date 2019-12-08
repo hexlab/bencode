bc63cc208902416575c5a827be275d6ce882668f
Kirill Karmadonov <kirill@live.com>
2019-12-08 11:36:58 +0200

    Add encoder

--- bencode/__init__.py
+++ bencode/__init__.py
@@ -1,1 +1,2 @@
 from .decode import bdecode
+from .encode import bencode
---
+++ bencode/encode.py
@@ -0,0 +1,26 @@
+def encoder(obj):
+    otype = type(obj)
+    if otype == str:
+        yield b'%d:%b' % (len(obj), obj.encode('utf8'))
+    elif otype == dict:
+        yield b'd'
+        for k in sorted(obj):
+            yield b'%d:%b' % (len(k), k.encode('utf8'))
+            yield from encoder(obj[k])
+        yield b'e'
+    elif otype == list:
+        yield b'l'
+        for i in obj:
+            yield from encoder(i)
+        yield b'e'
+    elif otype == bytes:
+        yield b'%d:%b' % (len(obj), obj)
+    elif otype in (int, bool):
+        yield b'i%de' % obj
+    else:
+        obj = str(obj).encode('utf8')
+        yield b'%d:%b' % (len(obj), obj)
+
+
+def bencode(obj):
+    return b''.join(encoder(obj))
